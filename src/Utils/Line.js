df71e0bdab04aa42c6dbcde0222a6f3faae60c3a
sonoogenesys <sonoo.gupta@ibism.com>
2022-09-24 17:50:48 +0530

    preview screen fixed

--- src/Utils/Line.js
+++ src/Utils/Line.js
@@ -1,5 +1,5 @@
 const Line = (props) => {
-    return(<div className={'border-top border-dark'} style={{marginLeft: '10px'}}/>)
+    return(<div className={'border-top border-dark'} style={{marginLeft: '25px'}}/>)
 }
 
 export default Line;
